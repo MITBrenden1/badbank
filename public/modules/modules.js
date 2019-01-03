32d768d203a7a7e36bf5c375057e1f051fea11b2
MITBrenden1 <46279594+MITBrenden1@users.noreply.github.com>
2019-01-03 22:11:00 +0000

    Removed Project name and replaced with Bad bank

--- public/modules/modules.js
+++ public/modules/modules.js
@@ -49,7 +49,7 @@ ui.navigation = `
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Bad Bank</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
