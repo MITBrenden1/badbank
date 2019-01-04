7fa979e48d3ac7bc89e7f4e555bf3d5b116fbac4
MITBrenden1 <46279594+MITBrenden1@users.noreply.github.com>
2019-01-04 10:58:21 +0000

    Changed Menu type to static

--- public/modules/modules.js
+++ public/modules/modules.js
@@ -33,62 +33,69 @@ ui.navigation = `
 
   <body>
 
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Bad Bank</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#createAccount">Create Account</a></li>
-            <li><a href="#login">Login</a></li>
-            <li><a href="#deposit">Deposit</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#withdrawal">Withdrawal</a></li>
-            <li><a href="#transactions">Transactions</a></li>
-            <li><a href="#balance">Balance</a></li>
-            <li><a href="#default">Default</a></li>
-            <li><a href="#alldata">All Data</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-
-    <!-- Include Bad Bank Logo -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">
-                    <img alt="Brand" src="bank.png">
-                </a>
-            </div>
-        </div>
-    </nav
-
     <div class="container">
 
-      <!-- Introductory welcome message -->
-      <div class="jumbotron">
-        <h1>Welcome to Bad Bank - where money is made easy</h1>
-        <p>Bad Bank - a new financial institution for easy money creation.</p>
-        <p>Disclaimer: This website is solely for the use for a Fire Hydrant Assignment.</p>
-      </div>
+        <!-- Static navbar -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
+                    <li><a href="../navbar-static-top/">Static top</a></li>
+                    <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+                </div><!--/.container-fluid -->
+            </nav>
+    
 
-    </div> <!-- /container -->
+        <!-- Include Bad Bank Logo -->
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">
+                            <img alt="Brand" src="bank.png">
+                        </a>
+                    </div>
+                </div>
+            </nav
+
+    
+
+        <!-- Introductory welcome message -->
+            <div class="jumbotron">
+                <h1>Welcome to Bad Bank - where money is made easy</h1>
+                <p>Bad Bank - a new financial institution for easy money creation.</p>
+                <p>Disclaimer: This website is solely for the use for a Fire Hydrant Assignment.</p>
+            </div>
+
+     </div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
